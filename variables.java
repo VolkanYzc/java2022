fac6718f725f854d59bc811ea0e502ab1bb56735
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 00:29:47 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+package javaHelloWorld;
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci Sayısı : ";
+		
+		System.out.println(mesaj+ogrenciSayisi);
+		System.out.println(mesaj+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+	}
+
+}
