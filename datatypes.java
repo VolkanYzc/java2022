15e8c1de6a732ed5485a437f6c73df31f16cf257
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 00:38:01 +0300

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+package javaHelloWorld;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		
+		
+		int sayi = 12;
+		double ondalikSayi = 13.2;
+		String sehir = "Ankara";
+		char karakter = 'A';
+		boolean varMı = false; 
+		
+		
+	}
+
+}
