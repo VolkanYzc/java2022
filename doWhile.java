a2bc8822ea32361dadeeb0b246a6c24e8f5561c9
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 01:39:41 +0300

    Add files via upload

---
+++ doWhile.java
@@ -0,0 +1,19 @@
+package javaHelloWorld;
+
+public class doWhile {
+
+	public static void main(String[] args) {
+		int j = 1;
+
+		do {
+			System.out.println(j);
+			j += 2;
+		}
+
+		while (j < 10);
+
+		System.out.println("Do-While Döngüsü Bitti");
+
+	}
+
+}
