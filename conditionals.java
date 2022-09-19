b3a86223d29b9325a0f644846e101a56bfaede33
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 00:46:53 +0300

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,28 @@
+package javaHelloWorld;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		
+		int sayi = 20;
+		
+		if (sayi<20) {
+			System.out.println("Sayı 20'den küçüktür");
+			
+		}
+		
+		else if (sayi>20) {
+			
+			System.out.println("Sayı 20'den büyüktür");
+			
+		} 
+		
+		else {
+			System.out.println("Sayı 20 ile eşittir");
+		}
+		
+		
+
+	}
+
+}
