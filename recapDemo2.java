0494418c1dac8673020f97e68912a509140a67b0
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 02:08:05 +0300

    Add files via upload

---
+++ recapDemo2.java
@@ -0,0 +1,21 @@
+package javaHelloWorld;
+
+public class recapDemo2 {
+
+	public static void main(String[] args) {
+		double[] mylist = { 1.2, 6.3, 4.3, 5.6 };
+		double total = 0;
+		double max = mylist[0];
+		for (double number : mylist) {
+			if (max < number) {
+				max = number;
+			}
+
+			total = total + number;
+			System.out.println(number);
+		}
+		System.out.println("Toplam " + total);
+		System.out.println("En Büyük " + max);
+	}
+
+}
