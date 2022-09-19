b584438601c518401c90b9bcd61554211a4d43c4
VolkanYzc <104868813+VolkanYzc@users.noreply.github.com>
2022-09-20 01:17:23 +0300

    Add files via upload

---
+++ loopDemo.java
@@ -0,0 +1,15 @@
+package javaHelloWorld;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+
+		for (int i = 2; i < 10; i += 2) {
+
+			System.out.println(i);
+
+		}
+		System.out.println("Döngü Bitti");
+	}
+
+}
