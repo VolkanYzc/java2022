2279a2ebc03e756064985073fc080cd16151215d
VolkanYzc <volkanyazici@outlook.com>
2022-10-09 04:26:32 +0300

    Create sesliHarfler.java

---
+++ sesliHarfler.java
@@ -0,0 +1,22 @@
+package javaHelloWorld;
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'I';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın sesli harf");
+			break;
+		default:
+			System.out.println("İnce sesli harf");
+			break;
+		}
+
+	}
+
+}
