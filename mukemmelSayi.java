fd9ce3323a5f115df16adc374c9d3897fb79a333
VolkanYzc <volkanyazici@outlook.com>
2022-10-09 04:26:30 +0300

    Create mukemmelSayi.java

---
+++ mukemmelSayi.java
@@ -0,0 +1,26 @@
+package javaHelloWorld;
+
+public class mukemmelSayi {
+
+	public static void main(String[] args) {
+		// 6 --> 1,2,3
+		// 25 --> 1,2,4,7,14
+
+		int number = 6;
+		int total = 0;
+
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				total = total + i;
+			}
+		}
+
+		if (total == number) {
+
+			System.out.println("Mükemmel Sayıdır");
+		} else {
+			System.out.println("Mükemmel Sayı değildir.");
+		}
+	}
+
+}
