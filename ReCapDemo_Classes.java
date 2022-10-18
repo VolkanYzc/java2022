3d94a66a9d4f18803f3b8bf027b36408f7bcd282
VolkanYzc <volkanyazici@outlook.com>
2022-10-18 03:32:21 +0300

    Create ReCapDemo_Classes.java

---
+++ ReCapDemo_Classes.java
@@ -0,0 +1,13 @@
+package javaHelloWorld;
+
+public class ReCapDemo_Classes {
+
+	public static void main(String[] args) {
+
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
